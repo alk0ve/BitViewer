a3ab4f72120691dc7abfb616fc790cc909e3e5d9
alexei-argus <alexei@argus-sec.com>
2017-03-13 09:16:05 +0200

    Rename "skip first bits" to "chop" :-(

--- BitViewer/Bit Viewer.Designer.cs
+++ BitViewer/Bit Viewer.Designer.cs
@@ -191,9 +191,9 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(582, 3);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.Size = new System.Drawing.Size(35, 13);
             this.label3.TabIndex = 12;
-            this.label3.Text = "Skip first bits:";
+            this.label3.Text = "Chop:";
             // 
             // vScrollBar1
             // 
--- README.txt
+++ README.txt
@@ -1,5 +1,7 @@
 Future plans:
  - make it faster/smoother
+	- simplify/optimize the rendering code
+	- make it double-buffered (?)
  - draw icon
 
 
